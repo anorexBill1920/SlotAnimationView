8c5b77e8d487fa5f17e6be9c903fe30705ea60c4
anorexbill <bryteanorchie1@gmail.com>
2023-09-29 11:44:30 -0600

    Comment Removed

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SlotAnimationView",
     platforms: [
-        .iOS(.v14)  // Specify the platforms and versions your package supports
+        .iOS(.v14) 
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
